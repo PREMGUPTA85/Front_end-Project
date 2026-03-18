c611b9c0d3fe61e10e0e1874857d1eb8ef0e546f
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-03-19 01:30:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
+const buttonEl = document.querySelector("#button");
+const todoList = document.querySelector("#todo_list");
+
+function addTodo() {
+    const li = document.createElement("li");
+    let inputValue = document.querySelector(".input").value;
+    const t = document.createTextNode(inputValue);
     li.appendChild(t);
 
     // Cheack input validation
