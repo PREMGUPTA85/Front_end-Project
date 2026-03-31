c66bf2ea1b2733c1c2ba65ba52982577c09eb9e5
Prem Kumar Gupta <premgupta100705@gmail.com>
2026-03-31 10:56:09 +0530

    Auto-sync: file changes

--- To_do_list/script.js
+++ To_do_list/script.js
@@ -1,34 +1,67 @@
 
-const buttonEl = document.querySelector("#button");
-const todoList = document.querySelector("#todo_list");
+// const buttonEl = document.querySelector("#button");
+// const todoList = document.querySelector("#todo_list");
 
-function addTodo() {
-    const li = document.createElement("li");
-    let inputValue = document.querySelector(".input").value;
-    const t = document.createTextNode(inputValue);
-    li.appendChild(t);
+// function addTodo() {
+//     const li = document.createElement("li");
+//     let inputValue = document.querySelector(".input").value;
+//     const t = document.createTextNode(inputValue);
+//     li.appendChild(t);
 
-    // Cheack input validation
-    if (inputValue === "") {
-        alert("Please enter a valid value");
-    } else {
-        todoList.appendChild(li);
+//     // Cheack input validation
+//     if (inputValue === "") {
+//         alert("Please enter a valid value");
+//     } else {
+//         todoList.appendChild(li);
+//     }
+//     console.log(inputValue);
+//     document.querySelector("input").value = "";
+// }
+
+// // add a remove button in each list item
+// function addRemoveButton(li) {
+//     const removeButton = document.createElement("button");
+//     removeButton.textContent = "Remove";
+//     removeButton.addEventListener("click", function() {
+//         todoList.removeChild(li);
+//     });
+//     li.appendChild(removeButton);
+// }
+// //it's html code is 
+
+
+
+// buttonEl.addEventListener("click", addTodo);
+
+
+// class method// class method
+let form1 = document.querySelector("#fi");
+let addinput1 = document.querySelector("#addtask");
+let tasklist1 = document.querySelector("#tasklist");
+
+form1.addEventListener("submit", (evt)=> {
+    evt.preventDefault();
+
+    let tasktext = addinput1.value.trim();
+    if(tasktext == ""){
+        alert("Please enter a task");
+        return;
     }
-    console.log(inputValue);
-    document.querySelector("input").value = "";
-}
 
-// add a remove button in each list item
-function addRemoveButton(li) {
-    const removeButton = document.createElement("button");
-    removeButton.textContent = "Remove";
-    removeButton.addEventListener("click", function() {
-        todoList.removeChild(li);
-    });
-    li.appendChild(removeButton);
-}
-//it's html code is 
+    let elementlist = document.createElement("li");
+    elementlist.textContent = tasktext;
+    tasklist1.appendChild(elementlist);
+    addinput1.value = ""; // clears the input box;
 
+    let elementdelete = document.createElement("button");
+    elementdelete.innerText = "x";
+    elementdelete.className = "deletebtn";
+    elementlist.append(elementdelete);
 
+});
 
-buttonEl.addEventListener("click", addTodo);
+tasklist1.addEventListener("click", (evt)=> {
+    if(evt.target.classList.contains("deletebtn")){
+        evt.target.parentElement.remove();
+    }
+});
