58ee3019db5fc91f21ff7b3ca2c4968c4e7620a9
Prem Kumar Gupta <premgupta100705@gmail.com>
2026-04-25 15:59:17 +0530

    Auto-sync: file changes

--- Weather App/script.js
+++ Weather App/script.js
@@ -3,4 +3,6 @@ http://api.weatherapi.com/v1/current.json?key=cd1898a995604cc6aa791727262504&q=M
 const fetchResults = async () => {
     let url = `http://api.weatherapi.com/v1/current.json?key=cd1898a995604cc6aa791727262504&q=Mumbai&aqi=no
 `
+
+    
 }
